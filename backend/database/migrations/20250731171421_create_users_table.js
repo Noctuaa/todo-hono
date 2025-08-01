c2940ffc7466dd1f67ce252c835c81d2602a6b2d
Noctua <titgeogeo@gmail.com>
2025-08-01 19:19:33 +0200

    feat:  add migrations for the users and tasks tables

---
+++ backend/database/migrations/20250731171421_create_users_table.js
@@ -0,0 +1,24 @@
+import { table } from "console";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = (knex) => {
+   return knex.schema.createTable('users', (table) => {
+      table.engine('InnoDB'); // Use InnoDB engine for MySQL
+      table.increments('id').primary();
+      table.string('username').notNullable().unique();
+      table.string('email').notNullable().unique();
+      table.string('password').notNullable();
+      table.timestamps(true, true); // created_at and updated_at
+   });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = (knex) => {
+   table.dropTableIfExists('users');
+};
